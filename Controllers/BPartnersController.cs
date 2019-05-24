af3f064a671f3affd764e15b3fa712864f48183a
YPHE\hhermawan <heri@yerryprimatama.com>
2019-05-24 14:25:38 +0700

    initial commit

---
+++ Controllers/BPartnersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using latihanazure.Models;
+
+namespace latihanazure.Controllers
+{
+    public class BPartnersController : ApiController
+    {
+        private BPartnerDBContext db = new BPartnerDBContext();
+
+        // GET: api/BPartners
+        public IQueryable<BPartner> GetbPartners()
+        {
+            return db.bPartners;
+        }
+
+        // GET: api/BPartners/5
+        [ResponseType(typeof(BPartner))]
+        public IHttpActionResult GetBPartner(int id)
+        {
+            BPartner bPartner = db.bPartners.Find(id);
+            if (bPartner == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(bPartner);
+        }
+
+        // PUT: api/BPartners/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutBPartner(int id, BPartner bPartner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != bPartner.BPartnerID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(bPartner).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BPartnerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/BPartners
+        [ResponseType(typeof(BPartner))]
+        public IHttpActionResult PostBPartner(BPartner bPartner)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.bPartners.Add(bPartner);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = bPartner.BPartnerID }, bPartner);
+        }
+
+        // DELETE: api/BPartners/5
+        [ResponseType(typeof(BPartner))]
+        public IHttpActionResult DeleteBPartner(int id)
+        {
+            BPartner bPartner = db.bPartners.Find(id);
+            if (bPartner == null)
+            {
+                return NotFound();
+            }
+
+            db.bPartners.Remove(bPartner);
+            db.SaveChanges();
+
+            return Ok(bPartner);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool BPartnerExists(int id)
+        {
+            return db.bPartners.Count(e => e.BPartnerID == id) > 0;
+        }
+    }
+}
---
+++ Models/BPartner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace latihanazure.Models
+{
+    public class BPartner
+    {
+        public int BPartnerID { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+
+    public class BPartnerDBContext : DbContext
+    {
+        public DbSet<BPartner> bPartners { get; set; }
+    }
+}
